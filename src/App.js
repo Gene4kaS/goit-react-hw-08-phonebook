fb78a346ce3555c7b0a43148143c8d10e9a565a5
Gene4kaS <jannasavchenko@gmail.com>
2022-01-01 15:54:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,23 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+    if (nextContacts.length > prevContacts.length && prevContacts.length !== 0);
+  }
+
   addContact = data => {
     const { contacts } = this.state;
     const { name, number } = data;
