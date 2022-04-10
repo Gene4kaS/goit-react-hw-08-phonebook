e42571ad7a3cdb7868c3b8d781ea27ed9c8e738e
Gene4kaS <jannasavchenko@gmail.com>
2022-04-10 14:58:45 +0300

    8-9

--- src/views/ContactsView/ContactsView.js
+++ src/views/ContactsView/ContactsView.js
@@ -3,11 +3,12 @@ import ContactForm from '../../components/ContactForm';
 import ContactsList from '../../components/ContactsList';
 import Filter from '../../components/Filter';
 import { ToastContainer } from 'react-toastify';
+import s from './ContactsView.module.css';
 
 export default function ContactsView() {
   return (
     <>
-      <div className="phonebook">
+      <div className={s.phonebook}>
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
